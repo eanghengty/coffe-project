cc35db0c416a4b12d299a167100c315f562978db
ty <hengty174@gmail.com>
2022-01-07 17:44:36 +0700

    finish

--- main.py
+++ main.py
@@ -35,18 +35,13 @@ def is_resource_sufficient(order_ingredients):
 
 def process_coins():
     print("please insert coin:")
-    total=int(input("how many quarter?: ") * 0.25)
-    total+=int(input("how many dimes?: ") * 0.1)
-    total+=int(input("how many nickles?: ") * 0.05)
-    total+= int(input("how many pennies?") * 0.01)
+    total=int(input("how many quarter?: ")) * 0.25
+    total+=int(input("how many dimes?: ")) * 0.1
+    total+=int(input("how many nickles?: ")) * 0.05
+    total+= int(input("how many pennies?")) * 0.01
     return total
 
-def is_transaction_successful(money_recieved,drink_cost):
-    if money_recieved >= drink_cost:
-        return True
-    else:
-        print("sorry that is not enough money")
-        return False
+
 
 profit=0
 resources={
@@ -56,6 +51,23 @@ resources={
     "money":profit
 }
 
+def make_coffee(drink_name, order_ingredient):
+        for item in order_ingredient:
+            drink_name[item] -= order_ingredient[item]
+        print(f"here is your {drink_name}. enjoy.")
+
+
+
+def is_transaction_successful(money_recieved,drink_cost):
+    if money_recieved >= drink_cost:
+        change = round(money_recieved - drink_cost,2)
+        print(f"here is this ${change} in change")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("sorry that is not enough money")
+        return False
 is_on = True
 
 while is_on:
@@ -66,9 +78,11 @@ while is_on:
         print(f'water : {resources["water"]}')
         print(f'milk : {resources["milk"]}')
         print(f'coffee : {resources["coffee"]}')
-        print(f'money: {resources["money"]}')
+        print(f'money: ${profit}')
     else:
         drink=MENU[choice]
         print(drink)
         if is_resource_sufficient(drink["ingredient"]):
             payment=process_coins()
+            if(is_transaction_successful(payment,drink["cost"])):
+                make_coffee(choice , drink["ingredient"])
